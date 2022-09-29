07e16ef2ae00b795f5ca97af174456376aa60c4c
Alex Oleynik <alexoleynik74@gmail.com>
2022-09-29 10:45:23 -0500

    changes to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,34 @@
-var names = [
-  "Tyler",
-  "Katy",
-  "Brent",
-  "Alex"
-]
+// Variables
+var movieInput = document.getElementById('search-movie');
+var submitBtn = documnet.getElementById('submitBtn')
 
-var pets = [
-  1, 
-  3,
-  0,
-  0,
-]
+//Add Api
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'fb53f80559msh64210164d6f770ap187dbfjsne3fdc83b8748',
+		'X-RapidAPI-Host': 'movie-database-alternative.p.rapidapi.com'
+	}
+};
+
+fetch(`https://movie-database-alternative.p.rapidapi.com/?s=${movieInput}&r=json&page=1`, options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
+submitBtn.addEventListener('search');
+
+
+
+
+
+
+
+
+
+// function getApi(event) {
+//   event.preventDefault()
+//   var movieName = movieInput.value
+//   //console.log(movieName)
+//   var requestUrl = 
+// }
