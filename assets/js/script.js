4188e975a9cf5c392e1177392ee2b39dd01cefc8
Tyler Arch <TylerArch@MacBook-Pro.local>
2022-09-29 10:43:20 -0500

    pseudocode for js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,23 @@
-var names = [
-  "Tyler",
-  "Katy",
-  "Brent",
-  "Alex"
-]
+//DOM Elements
 
-var pets = [
-  1, 
-  3,
-  0,
-  0,
-]
+
+//Global Variables
+
+
+//Functions
+//function to get API
+//function to populate search results
+//function to add button for adding to playlist
+//function to store playlist in local storage
+//function to remove from playlist
+//function to plug images from movie in to scrolling playlist
+
+
+
+
+
+//Event Listeners
+//Event listener for search button click
+//Event Listener for  going to my playlist button click
+//Event listener to add a movie to playlist
+
