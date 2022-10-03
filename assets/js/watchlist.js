f6d0af0faa1004780434775a3100ce3aa4023551
Tyler Olson <t19.olson@gmail.com>
2022-10-03 10:27:24 -0500

    cleaned up watchlist.js, removed un-needed code

--- assets/js/watchlist.js
+++ assets/js/watchlist.js
@@ -1,9 +1,4 @@
 
-
-
-
-
-
 //Global variables
 var movieInput = document.getElementById('search-movie');
 var searchForm = document.getElementById('searchForm')
@@ -25,113 +20,16 @@ const options = {
   }
 };
 
-// function getMovieAndSearch(event) {
-//   if (event) event.preventDefault()
-//   console.log(event.target)
-
-//   let movieName
-//   if (event.target.getAttribute("id") === "searchForm") {
-//     movieName = movieInput.value
-//     if (!movieSearch.includes(movieName)) movieSearch.push(movieName)
-//     storeSearch()
-//     renderSearch()
-//   } else if (event.target.getAttribute("id") === "get-info") {
-//     movieName = event.target.getAttribute("data-movie")
-//   }
-//   getMovie(movieName)
-// }
-
-// function getMovie(movieName) {
-//   fetch(`https://movie-database-alternative.p.rapidapi.com/?s=${movieName}&r=json&page=1`, options)
-//     .then(response => response.json())
-//     .then(data => {
-//       searchTitleEl.text(" " + movieName);
-//       //console.log(response.Search[0]);
-//       //let resultEl = $('<div>').text('test');
-//       const list = document.getElementById("result-content");
-
-//       while (list.hasChildNodes()) {
-//         list.removeChild(list.firstChild);
-//       }
-
-//       for (var i = 0; i < data.Search.length; i++) {
-//         //console.log(response.Search);
-//         let resultText = (data.Search[i].Title + " " + data.Search[i].Year);
-//         let textEl = $('<p>').text(resultText).addClass('col-12 searchTitle');
-//         let posterEl = $("<span>")
-//         if (data.Search[i].Poster !== "N/A") {
-//           posterEl = $('<img>').attr("src", data.Search[i].Poster).addClass('col -12 poster');
-//         }
-//         let saveToWatchListButton = $("<button>").text("Add to watchlist");
-
-//         let resultEl = $('<div>').append(textEl).append(posterEl).append(saveToWatchListButton).on("click", addToWatchList);
-//         // resultEl;
-//         resultsEl.append(resultEl);
-//       }
-//     })
-//     .catch(err => console.error(err));
-// }
-
-// searchForm.addEventListener('submit', getMovieAndSearch);
-
-// let addToWatchList = (event) => {
-//   event.preventDefault();
-
-//   let movieObject = {
-//     title: event.currentTarget.children[0].textContent,
-//     picture: event.currentTarget.children[1].src
-//   }
-//   //let title = event.currentTarget.children[0].textContent;
-//   //let picture = event.currentTarget.children[1].src;
-//   watchlist.push(movieObject)
-//   updateWatchlist()
-// }
-
 
 //Previous movie search saved to local storage
 function storeSearch() {
   localStorage.setItem("movieSearch", JSON.stringify(movieSearch));
 };
 
-// function renderSearch() {
+
   
-//   previousSearch.innerHTML = "";
-//   for (var i = 0; i < movieSearch.length; i++) {
-//     var singleSearch = movieSearch[i];
-//     var btn = document.createElement("button");
-//     btn.setAttribute("class", "get-info");
-//     btn.setAttribute("id", "get-info");
-//     var btn2 = document.createElement("button");
-//     btn.textContent = singleSearch;
-//     btn2.textContent = "X"
-//     btn.setAttribute("data-movie", movieSearch[i]);
-//     btn2.setAttribute("class", "delete-me");
-//     btn2.setAttribute("data-idx", i);
-//     previousSearch.appendChild(btn)
-//     previousSearch.appendChild(btn2)
-//   }
-// };
-
-// previousSearch.addEventListener('click', function (event) {
-//   if (event.target.matches(".delete-me")) {
-//     const idx = event.target.getAttribute("data-idx");
-//     movieSearch.splice(idx, 1);
-//     renderSearch();
-//     storeSearch();
-//   } else if (event.target.matches(".get-info")) {
-//     // const movieName = event.target.getAttribute("data-movie")
-//     movieInput.value = "";
-//     // console.log('click', event.target.getAttribute("data-movie"))
-//     getMovieAndSearch(event)
-//   }
-// });
 
 
-// //Clear list on HTML
-// clearBtn.addEventListener("click", function () {
-//   previousSearch.innerHTML = "";
-//   movieSearch = [];
-// });
 
 function updateWatchlist() {
   localStorage.setItem("watchlist", JSON.stringify(watchlist));
@@ -143,7 +41,7 @@ function startApp() {
   if (!watchlist) watchlist = []
   if (!movieSearch) movieSearch = []
   // console.log("watchlist retrieved from storage")
-//   renderSearch()
+  // renderSearch()
 }
 
 
@@ -152,38 +50,17 @@ function displayMovieCards(){
     var title = watchlist[i].title;
     var picture = watchlist[i].picture;
     
+    // building HTML elements and appending them
 
     var movieCard = document.createElement("li");
     movieCard.setAttribute("class", "glide__slide");
     var image = document.createElement("img");
     image.src = picture;
 
-    // movieCard.innerHTML = "<img src=" + picture + ">"
+   
     movieCard.appendChild(image)
-
-    // var final = movieCard + image
-
-    // // var movieCard = $()
-
-    // $('#slides').append('<li>' + final + '</li>');
-
-
-//      var mes = $('#message').val().replace('(<3)', '<img src="pics/lol/love.png">');
-//   $('#discussion').append('<li>' + mes + '</li>');
-
-
-
-//     var img = $('<img>').attr("src", picture);
-//     var movieCard = $("<li>").addClass("glide__slide" + img)
-// console.log(movieCard);
-
-
-    // var movieCard = $("<li class=glide__slide>" + "<img src=" + picture + "</li>")
- 
-    // var movieCard = $("<li>").addClass("glide__slide").append("<img>").attr("src", picture)
-
     slides.append(movieCard)
-    // console.log(movieCard)
+ 
   }
   
 }
