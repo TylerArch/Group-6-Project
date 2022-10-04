9fe08e4b6bd9653ed203aa8fc6c8d9479a06a186
Brent Gustafson <50030696+BrentG6000@users.noreply.github.com>
2022-10-04 12:14:03 -0500

    Added comments to watchlist.js

--- assets/js/watchlist.js
+++ assets/js/watchlist.js
@@ -1,17 +1,16 @@
-
 //Global variables
 var movieInput = document.getElementById('search-movie');
 var searchForm = document.getElementById('searchForm')
-let movieSearch = [];
 let previousSearch = document.getElementById('previous-search');
 let resultsEl = $("#result-content");
 let searchTitleEl = $("#result-text");
 var clearBtn = document.getElementById('clearBtn')
 var slides = document.getElementById("slides")
 
+let movieSearch = [];
 var watchlist = [];
 
-//Add Api
+// Api arguments
 const options = {
   method: 'GET',
   headers: {
@@ -27,28 +26,26 @@ function storeSearch() {
 };
 
 
-
+// Saves watchlist to local storage
 function updateWatchlist() {
   localStorage.setItem("watchlist", JSON.stringify(watchlist));
 }
 
+// Retrieves watchlist and movieSearch from local storage and saves them to arrays
 function startApp() {
   watchlist = JSON.parse(localStorage.getItem("watchlist"))
   movieSearch = JSON.parse(localStorage.getItem("movieSearch"))
   if (!watchlist) watchlist = []
   if (!movieSearch) movieSearch = []
-  // console.log("watchlist retrieved from storage")
-  // renderSearch()
 }
 
-
+// Creates slide elements using info from the watchlist array and displays them in the carousel
 function displayMovieCards(){
   for( var i = 0; i < watchlist.length; i++ ) {
     var title = watchlist[i].title;
     var picture = watchlist[i].picture;
     
     // building HTML elements and appending them
-
     var movieCard = document.createElement("li");
     movieCard.setAttribute("class", "glide__slide");
     var image = document.createElement("img");
